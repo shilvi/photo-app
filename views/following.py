fe2e735c9250f3ede812206d19627a3549f49157
Shilvi Satpati <rimli.satpati@gmail.com>
2022-02-05 22:58:11 -0600

    Finished following

--- views/following.py
+++ views/following.py
@@ -1,6 +1,8 @@
+from os import curdir
 from flask import Response, request
 from flask_restful import Resource
-from models import Following, User, db
+from models import Following, User, db, following
+from . import return_400_on_exception
 import json
 
 def get_path():
@@ -10,22 +12,39 @@ class FollowingListEndpoint(Resource):
     def __init__(self, current_user):
         self.current_user = current_user
     
+    @return_400_on_exception
     def get(self):
-        # Your code here
-        return Response(json.dumps([]), mimetype="application/json", status=200)
+        following = Following.query.filter(Following.user_id == self.current_user.id)
+        return Response(json.dumps([item.to_dict_following() for item in following]), mimetype="application/json", status=200)
 
+    @return_400_on_exception
     def post(self):
-        # Your code here
-        return Response(json.dumps({}), mimetype="application/json", status=201)
+        body = request.get_json()
+        following_id = body.get('user_id')
+        user_id = self.current_user.id
+
+        if not User.query.get(int(following_id)):
+            return Response(json.dumps({'message': f'User {following_id} does not exist'}), mimetype="application/json", status=404)
+
+        following = Following(user_id, following_id)
+        db.session.add(following)
+        db.session.commit()
+        return Response(json.dumps(following.to_dict_following()), mimetype="application/json", status=201)
 
 
 class FollowingDetailEndpoint(Resource):
     def __init__(self, current_user):
         self.current_user = current_user
     
+    @return_400_on_exception
     def delete(self, id):
-        # Your code here
-        return Response(json.dumps({}), mimetype="application/json", status=200)
+        following_query = Following.query.filter_by(id=id)
+        following = following_query.first()
+        if not following or following.user_id != self.current_user.id:
+            return Response(json.dumps({'message': 'Not Following'}), mimetype="application/json", status=404)
+        following_query.delete()
+        db.session.commit()
+        return Response(json.dumps({'message': 'Unfollowed'}), mimetype="application/json", status=200)
 
 
 def initialize_routes(api):
