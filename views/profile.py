e53fbbd2558d376ea4c89163cf4186e2b7e6a4cb
Shilvi Satpati <rimli.satpati@gmail.com>
2022-02-05 23:00:05 -0600

    Finished profile

--- views/profile.py
+++ views/profile.py
@@ -11,8 +11,7 @@ class ProfileDetailEndpoint(Resource):
         self.current_user = current_user
 
     def get(self):
-        # Your code here:
-        return Response(json.dumps({}), mimetype="application/json", status=200)
+        return Response(json.dumps(self.current_user.to_dict()), mimetype="application/json", status=200)
 
 
 def initialize_routes(api):
