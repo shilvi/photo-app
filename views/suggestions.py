0538fc4c718ef11eecbb086fe5d73398c28c50c7
Shilvi Satpati <rimli.satpati@gmail.com>
2022-02-05 23:15:38 -0600

    Finished suggestions

--- views/suggestions.py
+++ views/suggestions.py
@@ -1,6 +1,6 @@
 from flask import Response, request
 from flask_restful import Resource
-from models import User
+from models import User, Following
 from . import get_authorized_user_ids
 import json
 
@@ -10,8 +10,8 @@ class SuggestionsListEndpoint(Resource):
         self.current_user = current_user
     
     def get(self):
-        # Your code here:
-        return Response(json.dumps([]), mimetype="application/json", status=200)
+        suggestions = User.query.filter(User.id.not_in(get_authorized_user_ids(self.current_user))).limit(7)
+        return Response(json.dumps([item.to_dict() for item in suggestions]), mimetype="application/json", status=200)
 
 
 def initialize_routes(api):
