57037277d2f7b1879cbde05fe210d39fd80bd7b8
Shilvi Satpati <rimli.satpati@gmail.com>
2022-02-25 19:14:11 -0600

    Add hw05 starter code

--- app.py
+++ app.py
@@ -4,14 +4,25 @@ from flask import Flask, request
 from flask_restful import Api
 from flask_cors import CORS
 from flask import render_template
+# new import statements:
+import flask_jwt_extended  
+import decorators
 import os
 from models import db, User, ApiNavigator
 from views import bookmarks, comments, followers, following, \
     posts, profile, stories, suggestions, post_likes
+# new views:
+from views import authentication, token
 
 
 
 app = Flask(__name__)
+
+#JWT config variables and manager (add after app object created):
+app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET')
+app.config["JWT_TOKEN_LOCATION"] = ["headers", "cookies"]
+app.config["JWT_COOKIE_SECURE"] = False
+jwt = flask_jwt_extended.JWTManager(app)
 
 # CORS: allows anyone from anywhere to use your API:
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
@@ -38,6 +49,9 @@ post_likes.initialize_routes(api)
 profile.initialize_routes(api)
 stories.initialize_routes(api)
 suggestions.initialize_routes(api)
+# Initialize routes of 2 new views
+authentication.initialize_routes(app)
+token.initialize_routes(api)
 
 
 # Server-side template for the homepage:
@@ -48,13 +62,20 @@ def home():
         user=app.current_user
     )
 
+# Updated API endpoint includes a reference to 
+# access_token and csrf token.
 @app.route('/api')
+@decorators.jwt_or_login
 def api_docs():
-    navigator = ApiNavigator(app.current_user)
+    access_token = request.cookies.get('access_token_cookie')
+    csrf = request.cookies.get('csrf_access_token')
+    navigator = ApiNavigator(flask_jwt_extended.current_user)
     return render_template(
         'api/api-docs.html', 
-        user=app.current_user,
+        user=app.current_user,  #TODO: change to flask_jwt_extended.current_user
         endpoints=navigator.get_endpoints(),
+        access_token=access_token,
+        csrf=csrf,
         url_root=request.url_root[0:-1] # trim trailing slash
     )
 
