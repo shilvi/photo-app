537fc171972124ae4dc20e76d5f92f83522818b9
Shilvi Satpati <rimli.satpati@gmail.com>
2022-02-25 23:50:13 -0600

    Finish tokens

--- views/token.py
+++ views/token.py
@@ -1,4 +1,5 @@
 from models import User
+from views import return_400_on_exception
 import flask_jwt_extended
 from flask import Response, request
 from flask_restful import Resource
@@ -7,43 +8,65 @@ from datetime import timezone, datetime, timedelta
 
 class AccessTokenEndpoint(Resource):
 
+    @return_400_on_exception
     def post(self):
         body = request.get_json() or {}
-        print(body)
+        # print(body)
 
         # check username and log in credentials. If valid, return tokens
+        user = User.query.filter(User.username == body['username']).one_or_none()
+        if user is None:
+            return Response(json.dumps({ 
+                'message': 'Invalid username'
+            }), mimetype='application/json', status=401)
+        if not user.check_password(body['password']):
+            return Response(json.dumps({ 
+                'message': 'Invalid password'
+            }), mimetype='application/json', status=401)
+        expires = timedelta(hours=2)
+        access_token = flask_jwt_extended.create_access_token(
+            identity=user.id,
+            expires_delta=expires
+        )
+        expires = timedelta(hours=12)
+        refresh_token = flask_jwt_extended.create_refresh_token(
+            identity=user.id,
+            expires_delta=expires
+        )
         return Response(json.dumps({ 
-            "access_token": "???", 
-            "refresh_token": "???"
-        }), mimetype="application/json", status=200)
+            'access_token': access_token, 
+            'refresh_token': refresh_token
+        }), mimetype='application/json', status=200)
 
 
 class RefreshTokenEndpoint(Resource):
     
+    @return_400_on_exception
     def post(self):
 
         body = request.get_json() or {}
         refresh_token = body.get('refresh_token')
-        print(refresh_token)
+        # print(refresh_token)
         '''
         https://flask-jwt-extended.readthedocs.io/en/latest/refreshing_tokens/
         Hint: To decode the refresh token and see if it expired:
-        decoded_token = flask_jwt_extended.decode_token(refresh_token)
-        exp_timestamp = decoded_token.get("exp")
-        current_timestamp = datetime.timestamp(datetime.now(timezone.utc))
-        if exp_timestamp > current_timestamp:
-            return Response(json.dumps({ 
-                    "message": "refresh_token has expired"
-                }), mimetype="application/json", status=401)
-        else:
-            return Response(json.dumps({ 
-                    "access_token": "new access token goes here"
-                }), mimetype="application/json", status=200)
         '''
-        return Response(json.dumps({ 
-                "access_token": "new access token goes here"
-            }), mimetype="application/json", status=200)
-        
+        decoded_token = flask_jwt_extended.decode_token(refresh_token)
+        exp_timestamp = decoded_token.get('exp')
+        current_timestamp = datetime.timestamp(datetime.now(timezone.utc))
+        if exp_timestamp < current_timestamp:
+            return Response(json.dumps({ 
+                    'message': 'refresh_token has expired'
+                }), mimetype='application/json', status=401)
+        else:
+            expires = timedelta(hours=2)
+            access_token = flask_jwt_extended.create_access_token(
+                identity=decoded_token.get('sub'),
+                expires_delta=expires
+            )
+            return Response(json.dumps({ 
+                    'access_token': access_token
+                }), mimetype='application/json', status=200)
 
 
 def initialize_routes(api):
